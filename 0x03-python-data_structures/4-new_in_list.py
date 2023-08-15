df55fc3be2647b89b1518c1635596e2a74bfe826
Omarr-kh <omar1752003@gmail.com>
2023-08-15 04:36:14 -0700

    task 6, 8, 8, and 9

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
-    l = [x for x in my_list]
-    l[idx] = element
-    return (l)
+    new = [x for x in my_list]
+    new[idx] = element
+    return (new)
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+
+    maximum = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            maximum = my_list[i]
+
+    return (maximum)
