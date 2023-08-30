b3438d7848de171a47fd6ee9c939451d9c513a63
Omarr-kh <omar1752003@gmail.com>
2023-08-30 05:48:51 -0700

    task 100

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Node Model"""
+
+
+class Node:
+    """Node class for a singly linked list"""
+
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """singly linked list class"""
+
+    def __init__(self):
+        self.__head = None
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+        if self.__head = None:
+            new_node.next_node = None
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            temp = self.__head
+            while (temp.next_node is not None and
+                        temp.next_node.data < value):
+                temp = temp.next_node
+            new_node.next_node = temp.next_node
+            temp.next_node = new_node
+
+    def __str__(self):
+        values = []
+        temp = self.__head
+        while temp is not None:
+            values.append(str(temp.data))
+            temp = temp.next_node
+        return ('\n'.join(values))
