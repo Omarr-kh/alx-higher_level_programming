b90b73cab72c99c71b2985fd0092364fe7574c70
Omarr-kh <omar1752003@gmail.com>
2023-08-30 01:45:22 -0700

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square model"""
+
+
+class Square:
+    """Defines square"""
+
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size: length of a square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
