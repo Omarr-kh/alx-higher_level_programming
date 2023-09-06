ffac82eb6dcc7574a1c0759409a105a5881b951b
Omarr-kh <omar1752003@gmail.com>
2023-09-06 12:28:40 -0700

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -49,6 +49,8 @@ class Rectangle:
 
     def __str__(self) -> str:
         result = []
+        if self.__height == 0 or self.__width == 0:
+            return ""
         for i in range(self.__height):
             for j in range(self.__width):
                 result.append("#")
