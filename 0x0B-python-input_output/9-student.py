24d93e59bfb3dbbf434265297641c7d07ff67c66
Omarr-kh <omar1752003@gmail.com>
2023-09-12 05:58:46 -0700

    tasks 7 to 10

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name: (str) first name of the student.
+            last_name: (str) last name of the student.
+            age: (int) age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student"""
+        return self.__dict__
