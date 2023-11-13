cd65925f28433fbaae424f5a77df576dc9047863
Omarr-kh <omar1752003@gmail.com>
2023-11-13 11:05:20 -0800

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort()[arr.length - 2];
+  const second = arr.sort(function (a, b) { return b - a; })[1];
   console.log(second);
 }
