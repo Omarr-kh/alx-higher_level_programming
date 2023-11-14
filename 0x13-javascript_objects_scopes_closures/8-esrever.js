c5dd11a20a4fed06a5eb8f33726caa8ec91fb585
Omarr-kh <omar1752003@gmail.com>
2023-11-14 11:02:14 -0800

    tasks completed

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/user/bin/node
+exports.esrever = function (list) {
+  let len = list.length - 1;
+  let i = 0;
+  while ((len - i) > 0) {
+    const temp = list[len];
+    list[len] = list[i];
+    list[i] = temp;
+    i++;
+    len--;
+  }
+  return list;
+};
