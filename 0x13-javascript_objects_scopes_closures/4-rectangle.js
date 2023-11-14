7a4537cf578d8f3bcb53a9537394b4d6dad240a3
Omarr-kh <omar1752003@gmail.com>
2023-11-14 10:38:01 -0800

    tasks 3, and 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rec = '';
+      for (let j = 0; j < this.width; j++) {
+        rec += 'X';
+      }
+      console.log(rec);
+    }
+  }
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+module.exports = Rectangle;
