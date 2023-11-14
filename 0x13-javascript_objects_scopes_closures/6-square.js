3224a05e9edf75992a519db22278b3d3ddb960ab
Omarr-kh <omar1752003@gmail.com>
2023-11-14 10:47:54 -0800

    tasks complete

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const SquarePre = require('./5-square');
+
+class Square extends SquarePre {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let rec = '';
+      for (let j = 0; j < this.width; j++) {
+        rec += 'X';
+      }
+      console.log(rec);
+    }
+  }
+}
+
+module.exports = Square;
