d9f772c5b3c1054e775968dca7f21af8051ec9d8
Omarr-kh <omar1752003@gmail.com>
2023-11-19 12:57:53 -0800

    bdaaaan

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for inst in session.query(State).filter(State.name.like('%a%')):
-        print(inst.id, inst.name, sep=": ")
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        print(instance.id, instance.name, sep=": ")
