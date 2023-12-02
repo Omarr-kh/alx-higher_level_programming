5030958bb69368383621af885c8cc2bc0bd4e560
Omarr-kh <omar1752003@gmail.com>
2023-12-02 05:52:19 -0800

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+    script that takes in a letter and sends a POST request to 
+    http://0.0.0.0:5000/search_user with the letter as a parameter.
+'''
+import sys
+import requests
+
+
+def main():
+    data = {"q": argv[1] if len(argv) > 1 else ""}
+    response = requests.post('http://0.0.0.0:5000/search_user', data=data)
+
+    try:
+        data_json = response.json()
+        if 'id' in dict_json and 'name' in dict_json:
+            print('[{}] {}'.format(data_json['id'], data_json['name']))
+        else:
+            print('No result')
+    except Exception:
+        print('Not a valid JSON')
+
+
+if __name__ == "__main__":
+    main()
