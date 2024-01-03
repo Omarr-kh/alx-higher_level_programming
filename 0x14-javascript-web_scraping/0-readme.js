673e43259c2793f80ca1c7a31dcd8d0389e1f6fe
Omarr-kh <omar1752003@gmail.com>
2024-01-03 00:32:44 -0800

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+
+fs.readFile(process.argv[2], 'utf-8', (error, content)=> {
+    if (error) {
+        console.log('Error reading the file:', error);
+    } else {
+        console.log(content);
+    }
+});
