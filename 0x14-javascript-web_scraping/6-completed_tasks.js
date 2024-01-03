47b0e63d44be838cefaddb23912bc51d45016bd4
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-01-03 20:15:38 +0200

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
 const urlAPI = process.argv[2];
 
-request(urlAPI, function (error, response, body) {
+request.get(urlAPI, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else if (response.statusCode === 200) {
-    const completedTasks = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completedTasks === true) {
-        if (completedTasks[task.userId] === undefined) {
-          completedTasks[task.userId] = 1;
-        } else {
-          completedTasks[task.userId]++;
-        }
-      }
-    }
-    console.log(completedTasks);
-  } else {
-    console.log('Error. Status code: ' + response.statusCode);
+    return;
   }
+  const todos = JSON.parse(body);
+  const usersData = {};
+  for (const todo of todos) {
+    if (!(todo.userId in usersData) && todo.completed) {
+      usersData[todo.userId] = 1;
+    } else if (todo.completed) {
+      usersData[todo.userId] += 1;
+    }
+  }
+  console.log(usersData);
 });
